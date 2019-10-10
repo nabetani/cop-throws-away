552be0bb97d9594624fd1a9eb12069389b46dfa0
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-10-10 14:34:35 +0900

    add sample

---
+++ sample.rb
@@ -0,0 +1,14 @@
+
+# frozen_string_literal: true
+
+def header
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
