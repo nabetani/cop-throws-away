3e1de8112f3f1b2079ed358246b809b11ba469f4
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-10-10 14:55:29 +0900

    add nonsense lambdas

--- sample.rb
+++ sample.rb
@@ -11,3 +11,401 @@ def header
     */
   HEADER
 end
+
+# I wrote following lambdas to make screen update slower
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
+
+lambda do
+  <<~HEADER
+    /**
+     @author NABETANI Takenori
+     @copyright Copyright© 2019 NABETANI Takenori 
+  
+     This line has 5 leading spaces, but previous line has two spaces.
+     This software is distributed under MIT License
+    */
+  HEADER
+end
